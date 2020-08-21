f91161c1723aa01ebd660bc09d2d0fdec659e984
Jesse Zarat <jczarat@gmail.com>
2020-08-20 18:59:51 -0600

    config database and add model

---
+++ models/flight.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {type: String, enum: ['American', 'Delta', 'Southwest', 'United']},
+    airport: {type: String, default: 'DEN', enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN']},
+    flightNo: {type: Number, min: 10, max: 9999},
+    departs: {
+        type: Date,
+        default: function(){
+        const date = new Date();
+        date.setFullYear(date.getFullYear() + 1);
+        return date;
+        }
+    }
+})
