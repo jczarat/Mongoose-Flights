caa4d81c04bf2f898bd1dd0bcc4a0d4f3a781f84
Jesse Zarat <jczarat@gmail.com>
2020-08-25 17:28:08 -0600

    add ticket functionality

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -2,9 +2,26 @@ const Flight = require('../models/flight')
 const Ticket = require('../models/ticket')
 
 module.exports = {
+    new: newTicket,
     create
 }
 
+function newTicket(req, res) {
+    Flight.findById(req.params.id, function(err, flight){
+        console.log(flight)
+        res.render('tickets/new', {title: 'Add Ticket', flight})
+    })
+}
+
+// function create (req, res){
+//     Flight.findById(req.params.id, function(err, flight){
+        
+//     })
+// }
+
 function create (req, res){
-    
+    req.body.flight = req.params.id
+    console.log('Hello World')
+    Ticket.create(req.body)
+    .then(()=> {res.redirect(`/flights/${req.params.id}`)})
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,7 +5,6 @@ const flightsCtrl = require('../controllers/flights')
 /* GET users listing. */
 
 router.get('/', flightsCtrl.index);
-
 router.get('/new', flightsCtrl.new);
 router.post('/', flightsCtrl.create);
 router.get('/:id', flightsCtrl.show);
